c8ff686d2930b4d89ac9c4b22f74c07331921dc1
astra-kawa <mjagodzski@gmail.com>
2025-10-04 23:54:47 -0400

    added sorting children nodes by rank

--- src/domain/node_graph.rs
+++ src/domain/node_graph.rs
@@ -13,15 +13,21 @@ impl NodeGraph {
 
         // basic nested for loop, improve with better logic later
         for parent_node in &nodes {
-            let mut children_ids: Vec<Uuid> = Vec::new();
+            let mut children_rank_ids: Vec<(String, Uuid)> = Vec::new();
 
             for child_node in &nodes {
                 if let Some(childs_parent_id) = child_node.parent_id() {
                     if parent_node.id() == childs_parent_id {
-                        children_ids.push(child_node.id());
+                        children_rank_ids.push((child_node.rank_key_str(), child_node.id()));
                     }
                 }
             }
+
+            if !children_rank_ids.is_empty() {
+                children_rank_ids.sort_by(|a, b| a.0.cmp(&b.0));
+            }
+
+            let children_ids = children_rank_ids.into_iter().map(|(_, id)| id).collect();
 
             children_by_parent.insert(parent_node.id(), children_ids);
         }
